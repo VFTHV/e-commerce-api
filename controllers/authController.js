ba4db492a1064cf36f5d027ca38933910f818c7b
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-22 21:46:37 -0600

    added jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -17,8 +18,13 @@ const register = async (req, res) => {
   let role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ email, name, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
 
-  res.status(StatusCodes.CREATED).send(user);
+  const token = jwt.sign(tokenUser, 'jwtSecret', {
+    expiresIn: '1d',
+  });
+
+  res.status(StatusCodes.CREATED).send({ user: tokenUser, token });
 };
 
 const login = async (req, res) => {
