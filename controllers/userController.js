633d8820f523d189eb430602cf3cb1e9c757d469
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-29 21:46:09 -0600

    Get single user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,24 +3,19 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 
 const getAllUsers = async (req, res) => {
-  const users = await User.find({ role: 'uuser' });
+  const users = await User.find({ role: 'user' }).select('-password');
 
-  if (!users.length) {
-    throw new CustomError.NotFoundError('Nothing found');
-  }
-
-  const usersNoPassword = users.map((user) => {
-    const { _id, name, email, role } = user;
-    return { _id, name, email, role };
-    // return { ...user, password: 'hidden' };
-  });
-
-  res.status(StatusCodes.OK).send({ users: usersNoPassword });
+  res.status(StatusCodes.OK).send({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send('getSingleUser');
-  console.log(req.params);
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
+  }
+
+  res.status(StatusCodes.OK).send({ user });
 };
 
 const showCurrentUser = async (req, res) => {
