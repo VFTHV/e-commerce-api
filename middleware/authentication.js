d278f0fd131c00d1e2683812d9053dd5819010f6
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-30 19:25:56 -0600

    Basic authorizePermissions

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,15 +1,26 @@
-const CustomErr = require('../errors');
+const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
+
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+};
+
+const authorizePermissions = (req, res, next) => {
+  console.log('admin route');
   next();
 };
 
-module.exports = authenticateUser;
+module.exports = { authenticateUser, authorizePermissions };
