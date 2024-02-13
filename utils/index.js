b452ef3e85c7a4c3c67415cd8efe7f0748d09c6b
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-13 14:59:50 -0600

    checkPermissions helper in getUser

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const { isTokenValid, createJWT, attachCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
+const checkPermissions = require('./checkPermissions');
 
 module.exports = {
   isTokenValid,
   createJWT,
   attachCookiesToResponse,
   createTokenUser,
+  checkPermissions,
 };
