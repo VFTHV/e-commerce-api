9bf72674555cdb3afbede3300f14b7920b7caef0
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-15 20:47:47 -0600

    Running validators

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,6 +32,7 @@ const updateProduct = async (req, res) => {
   // running validators
   // running validators
   // running validators
+  // running validators
   const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
     new: true,
     runValidators: true,
