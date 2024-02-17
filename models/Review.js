64abb3c2893b6bdc168fc14e74fb1efb407c7030
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-16 21:29:01 -0600

    Review model with rating

--- models/Review.js
+++ models/Review.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const ReviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Review', ReviewSchema);
