05df2ae00431b4858663c39d94eda428f69d4ab3
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-23 22:50:01 -0600

    unauthorized error

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -8,6 +8,7 @@ const checkPermissions = (requestUser, resourceUserId) => {
   if (requestUser.role === 'admin') return;
   if (requestUser.userId === resourceUserId.toString()) return;
   // unauthorized error
+  // unauthorized error
   throw new CustomError.UnauthorizedError(
     'Not authorized to access this route'
   );
