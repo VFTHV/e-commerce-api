cada485e73d4ac7def53138ee45e63f13eeeec40
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-02 22:01:30 -0600

    basic reviewRoutes

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+const {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviewController');
+
+router
+  .route('/createReview')
+  .post(authenticateUser, authorizePermissions('user'), createReview);
+
+router.route('/getAllReviews').get(getAllReviews);
+router.route('/getSingleReview').get(getSingleReview);
+router
+  .route('/updateReview')
+  .patch(authenticateUser, authorizePermissions('user'), updateReview);
+
+router
+  .route('/deleteReview')
+  .delete(authenticateUser, authorizePermissions('user'), deleteReview);
