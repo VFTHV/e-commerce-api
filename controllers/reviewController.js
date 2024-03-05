ed7652cbd5f40a6d8a7cb720ac027b4b5c655464
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-04 22:02:49 -0600

    Basic createReview controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,21 @@
+const CustomError = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+const Review = require('../models/Review');
+
 const createReview = async (req, res) => {
-  res.send(req.body);
+  const { product } = req.body;
+
+  if (!product) {
+    throw new CustomError.BadRequestError('Product not found');
+  }
+
+  req.body.user = req.user.userId;
+
+  const review = await Review.create({ review: req.body.review });
+
+  res.status(StatusCodes.CREATED).json(req.body);
 };
+
 const getAllReviews = async (req, res) => {
   res.send('getAllReviews');
 };
