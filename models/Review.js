abd778553ecb94ec651995156f1ee6936f390302
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-17 20:08:35 -0500

    console error on aggregation pipeline

--- models/Review.js
+++ models/Review.js
@@ -63,11 +63,6 @@ ReviewSchema.statics.calculateAverageRating = async function (productId) {
     );
   } catch (error) {
     console.log(error);
-    console.log(error);
-    console.log(error);
-    console.log(error);
-    console.log(error);
-    console.log(error);
   }
 };
 
