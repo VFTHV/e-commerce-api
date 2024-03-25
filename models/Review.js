e9984369406ff74a1aee0dbf950beee5e9962eb0
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-24 22:09:16 -0500

    checking the result

--- models/Review.js
+++ models/Review.js
@@ -54,6 +54,7 @@ ReviewSchema.statics.calculateAverageRating = async function (productId) {
   console.log(result);
   console.log(result);
   console.log(result);
+  console.log(result);
 
   try {
     await this.model('Product').findOneAndUpdate(
