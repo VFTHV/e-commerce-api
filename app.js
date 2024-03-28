09d0b1c505d97684d748732f07dd4eac8d8054de
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-27 21:48:23 -0500

    installed security packages

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
 // database
 const connectDB = require('./db/connect');
 
@@ -24,6 +30,18 @@ const orderRouter = require('./routes/orderRoutes');
 // middleware
 const notFountMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny')); // logs every route request on console
 app.use(express.json());
